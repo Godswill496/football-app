0a21dd124d625ce34a71194e224241b48372b338
godswill <20102525@mail.wit.ie>
2025-11-27 17:59:53 +0000

    JUnit Tests for listing notes from the collection

--- src/main/kotlin/controllers/MatchPlayerController.kt
+++ src/main/kotlin/controllers/MatchPlayerController.kt
@@ -2,18 +2,34 @@ package ie.setu.controllers
 
 import ie.setu.models.MatchPlayer
 
-
 class MatchPlayerController {
 
-    private val matchPlayers = mutableListOf<MatchPlayer>()
+    private var matchPlayers = ArrayList<MatchPlayer>()
     private var lastId = 0
-    private fun getId() = lastId++
+    private fun getNextId() = lastId++
 
-    fun addPlayerToMatch(matchPlayer: MatchPlayer) {
-        matchPlayer.id = getId()
-        matchPlayers.add(matchPlayer)
+    fun addPlayerToMatch(matchPlayer: MatchPlayer): Boolean {
+        matchPlayer.id = getNextId()
+        return matchPlayers.add(matchPlayer)
     }
 
-    fun listPlayersInMatch(matchId: Int) =
-        matchPlayers.filter { it.matchId == matchId }
+    fun listPlayersInMatch(matchId: Int): List<MatchPlayer> {
+        return matchPlayers.filter { it.matchId == matchId }
+    }
+
+    fun numberOfMatchPlayers(): Int {
+        return matchPlayers.size
+    }
+
+    fun findMatchPlayer(index: Int): MatchPlayer? {
+        return if (isValidListIndex(index, matchPlayers)) {
+            matchPlayers[index]
+        } else null
+    }
+
+    fun isValidListIndex(index: Int, list: List<Any>): Boolean {
+        return (index >= 0 && index < list.size)
+    }
 }
+
+
